cc84ce3789a59131b505a362add08948f0146322
Novoselov Anton <nantog@gmail.com>
2018-10-02 16:26:03 +0300

    ScrapyBooks added

---
+++ ScrapyCourse/ScrapyBooks/books_crawler/spiders/books.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class BooksSpider(CrawlSpider):
+    name = 'books'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+
+    rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow = True),)
+
+    def parse_page(self, response):
+        pass
